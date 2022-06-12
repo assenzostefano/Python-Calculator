cf381aa9303b63f50cb6af473e3f0fedf4420c4b
Stefano <68437687+assenzostefano@users.noreply.github.com>
2022-06-12 17:17:56 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,57 +1,71 @@
-while True:
-    print('''
-    Welcome to the calculator program!
-    Created by: Stef58
-    Below is a list of the various functions available:
+import math
 
-    -To perform an Addition, select 1;
-    -To perform a Subtraction, select 2;
-    -To perform a Multiplication, select 3;
-    -To perform a Split, select 4;
-    -To perform an Exponential Calculation, select 5;
-    -To exit the program you can type ESC;
-    ''')
+print("Benvenuto nel calcolatore supremo cosa vuoi fare?")
 
-    scelta = input('Enter the number corresponding to the selected operation --->  ')
-    if scelta == "1":
-        print('\nYou have chosen: Addition\n')
-        a = float(input('Enter the First Number -> '))
-        b = float(input('Enter the Second Number -> '))
-        print('The sum result is: ' + str(a + b))
-    elif scelta == "2":
-        print('\nYou have chosen: Subtraction\n')
-        a = float(input('Enter the First Number -> '))
-        b = float(input('Enter the Second Number -> '))
-        print('Il risultato della Sottrazione è: ' + str(a - b))
-    elif scelta == "3":
-        print('\nHai scelto: Moltiplicazione\n')
-        a = float(input('Enter the First Number -> '))
-        b = float(input('Enter the Second Number -> '))
-        print('Il risultato della Moltiplicazione è: ' + str(a * b))
-    elif scelta == "4":
-        print('\nHai scelto: Divisione\n')
-        a = float(input('Enter the First Number -> '))
-        b = float(input('Enter the Second Number -> '))
-        print('Il risultato della Divisione è: ' + str(a / b))
-    elif scelta == "5":
-        print('\nHai scelto: Calcolo Esponenziale\n')
-        a = float(input('Insert the Base -> '))
-        b = float(input('Enter the Exponent -> '))
-        print('The result of the Exponential Calculation is: ' + str(a ** b))
-    elif scelta == "ESC":
-        print('''The application will now be closed!
+def calculate():
+    print("1. Addizione")
+    print("2. Sottrazione")
+    print("3. Moltiplicazione")
+    print("4. Divisione")
+    print("5. Radice quadrata")
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++''')
-        break
+    selezione = int(input("Seleziona una opzione: "))
 
-    loop = input('\nYou want to continue using the application? Y/N ')
-    if loop == "Y" or loop == "y":
-        print('''I'm going back to the main menu!
+#Addizione
+    if selezione == 1:
+        number1 = int(input("Scrivi il primo numero: "))
+        number2 = int(input("Scrivi il secondo numero: "))
+        number_1 = number1
+        number_2 = number2
+        addizione = number_1 + number_2 
+        print(addizione)
+        dinuovo()
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++''')
-        continue
-    else:
-        print('''Thank you and goodbye!
+#Sottrazione
+    elif selezione == 2:
+        number1 = int(input("Scrivi il primo numero: "))
+        number2 = int(input("Scrivi il secondo numero: "))
+        number_1 = number1
+        number_2 = number2
+        sottrazione = number_1 - number_2
+        print(sottrazione)
+        dinuovo()
 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++''')
-        break
+#Moltiplicazione
+    elif selezione == 3:
+        number1 = int(input("Scrivi il primo numero: "))
+        number2 = int(input("Scrivi il secondo numero: "))
+        number_1 = number1
+        number_2 = number2
+        moltiplicazione = number_1 * number_2
+        print(moltiplicazione)
+        dinuovo()
+
+#Divisione
+    elif selezione == 4:
+        number1 = int(input("Scrivi il primo numero: "))
+        number2 = int(input("Scrivi il secondo numero: "))
+        number_1 = number1
+        number_2 = number2
+        divisione = number_1 / number_2
+        print(divisione)
+        dinuovo()
+
+#Radice quadrata
+    elif selezione == 5:
+        number1 = int(input("Scrivi il primo numero: "))
+        number_1 = number1
+        radice = math.sqrt(number_1)
+        print(radice)
+        dinuovo()
+
+#Calcolare ancora
+def dinuovo():
+    selezione = input("Vuoi calcolare ancora?")
+    if selezione == 'Y' or selezione == 'y' or selezione == 'yes' or selezione == 'Yes' or selezione == 'YES':
+        calculate()
+
+    elif selezione == 'N' or selezione == 'n' or selezione == 'no' or selezione == 'No' or selezione == 'NO':
+        print('Ci vediamo!')
+
+calculate()
